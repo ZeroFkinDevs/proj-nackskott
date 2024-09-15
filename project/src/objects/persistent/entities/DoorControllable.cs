57604c191981754f1d77b2926b0d0e4f43a702a8
AshenHermit <nameless.voice.x@gmail.com>
2024-09-15 15:38:34 +0300

    partial feat: door lock

--- project/src/objects/persistent/entities/DoorControllable.cs
+++ project/src/objects/persistent/entities/DoorControllable.cs
@@ -5,6 +5,34 @@ namespace Game
 {
     public partial class DoorControllable : RigidBody3D, IPointerControlling
     {
+        [Export]
+        private bool _locked = false;
+        public bool Locked{get{return _locked;} set{_locked = value; UpdateJoint(); }}
+
+        [Export]
+        public Joint3D LockedJoint;
+        [Export]
+        public Joint3D OpenJoint;
+
+        public void UpdateJoint(){
+            Joint3D jointdisable = null;
+            Joint3D jointenable = null;
+            if(_locked){
+                jointdisable = OpenJoint;
+                jointenable = LockedJoint;
+            }else{
+                jointdisable = LockedJoint;
+                jointenable = OpenJoint;
+            }
+            if(jointdisable!=null) jointdisable.NodeA = null;
+            if(jointenable!=null) jointenable.NodeA = GetPath();
+        }
+
+        public override void _Ready()
+        {
+            Locked = _locked;
+        }
+
         public override void _PhysicsProcess(double delta)
         {
             var vel = AngularVelocity;
